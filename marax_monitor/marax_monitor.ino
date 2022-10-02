9e3afdae31cd27baa44cc7c69af966312dc3e366
DirkDiggler8 <114864237+DirkDiggler8@users.noreply.github.com>
2022-10-02 21:07:42 +0800

    Update marax_monitor.ino
    - show different layout for Mara Off, Pump Off, Pump On state

--- marax_monitor/marax_monitor.ino
+++ marax_monitor/marax_monitor.ino
@@ -16,20 +16,39 @@ int d5 = 5; //orange PIN 4 Mara TX to Arduino RX D5
 int d6 = 6; //black  PIN 3 Mara RX to Arduino TX D6
 
 //Internals
-long lastMillis;
+long lastPumpOnMillis = 0;
 int seconds = 0;
 int lastTimer = 0;
 long serialTimeout = 0;
 char buffer[BUFFER_SIZE];
 int index = 0;
+int isMaraOff = 0;
+long lastToggleTime = 0;
+int HeatDisplayToggle = 0;
+
+const int Sim = 0;
 
 //Mara Data
 String maraData[7];
-String* lastMaraData;
 
 //Instances
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 SoftwareSerial mySerial(d5, d6);
+
+void SetSim()
+{
+  if (Sim == 1)
+  {
+    //C1.06,116,124,093,0840,1,0
+    maraData[0] = String("C1.06");
+    maraData[1] = String("116");
+    maraData[2] = String("124");
+    maraData[3] = String("093");
+    maraData[4] = String("0840");
+    maraData[5] = String("1");
+    maraData[6] = String("0");
+  }
+}
 
 void setup()
 {
@@ -60,11 +79,13 @@ void getMaraData()
 
   while (mySerial.available())
   {
+    isMaraOff = 0;
     serialTimeout = millis();
     char rcv = mySerial.read();
     if (rcv != '\n')
       buffer[index++] = rcv;
-    else {
+    else 
+    {
       index = 0;
       Serial.println(buffer);
       char* ptr = strtok(buffer, ",");
@@ -74,13 +95,15 @@ void getMaraData()
         maraData[idx++] = String(ptr);
         ptr = strtok(NULL, ",");
       }
-      lastMaraData = maraData;
     }
   }
   if (millis() - serialTimeout > 6000)
   {
+    isMaraOff = 1;
+    SetSim();
     serialTimeout = millis();
     mySerial.write(0x11);
+
   }
 }
 
@@ -88,84 +111,127 @@ void updateView()
 {
   display.clearDisplay();
   display.setTextColor(WHITE);
-  //HX
-  display.setCursor(2, 2);
-  display.setTextSize(2);
-  display.print(lastMaraData[3].toInt());
-  display.setTextSize(1);
-  display.print((char)247);
-  display.setTextSize(1);
-  display.print("C");
-  //Pump
-  display.setCursor(2, 30);
-  display.print("H");
-  if (lastMaraData[5].toInt() == 0)
-    display.drawCircle(17, 33, 6, WHITE);
-  else
-    display.fillCircle(17, 33, 5, WHITE);
-  display.setCursor(30, 30);
-  //Heat
-  display.print("P");
-  if (lastMaraData[6].toInt() == 0)
-    display.drawRect(40, 28, 10, 10, WHITE);
-  else
-    display.fillRect(40, 28, 10, 10, WHITE);
-  //Steam
-  display.setCursor(2, 50);
-  display.setTextSize(2);
-  display.print(lastMaraData[1].toInt());
-  display.setTextSize(1);
-  display.print((char)247);
-  display.setTextSize(1);
-  display.print("C");
-  display.drawLine(55, 0, 55, 68, WHITE);
-  display.setCursor(65, 15);
-  display.setTextSize(5);
-  //Timer
-  if (seconds > 3)
+
+  if (seconds == 0)
   {
-    String actual = String(seconds);
-    if (actual.length()< 2)
-      actual = "0" + actual;
-    display.print(actual);
-  }
-  else if (lastTimer > 3)
-  {
-    String last = String(lastTimer);
-    if (last.length()< 2)
-      last = "0" + last;
-    display.print(last);
-  }
+    if (isMaraOff == 1)
+    {
+      display.setCursor(30, 30);
+      display.setTextSize(4);
+      display.print("OFF");
+    }
+    else
+    {
+      //HX
+      display.setCursor(2, 30);
+      display.setTextSize(4);
+      display.print(maraData[3].toInt());
+      display.setTextSize(1);
+      display.print((char)247);
+      display.setTextSize(1);
+      display.print("C");
+
+      //Steam
+      display.setCursor(80, 30);
+      display.setTextSize(2);
+      display.print(maraData[1].toInt());
+      display.setTextSize(1);
+      display.print((char)247);
+      display.setTextSize(1);
+      display.print("C");
+
+      //Draw Line
+      display.drawLine(74, 0, 74, 64, WHITE);
+      display.drawLine(0, 18, 128, 18, WHITE);
+
+      //Target
+      display.setCursor(80, 2);
+      display.setTextSize(2);
+      display.print(maraData[2].toInt());
+      display.setTextSize(1);
+      display.print((char)247);
+      display.setTextSize(1);
+      display.print("C");
+
+      //Boiler
+      display.setCursor(8, 5);
+      display.setTextSize(1);
+      
+      if (maraData[5].toInt() == 1)
+      {
+        if ((millis() - lastToggleTime) > 1000)
+        {
+          lastToggleTime = millis();
+          if (HeatDisplayToggle == 1)
+          {
+            HeatDisplayToggle = 0;
+          }
+          else
+          {
+            HeatDisplayToggle = 1;
+          }
+        }
+        if (HeatDisplayToggle == 1)
+        {
+          display.print("Heating...");
+        }
+        else
+        {
+          display.print("");
+        }
+        display.fillRect(0, 0, 4, 14, WHITE);
+      }
+      else
+      {
+        display.print("");
+        display.fillRect(0, 0, 4, 14, BLACK);
+      }
+
+      //Mode
+      display.setTextSize(1);
+      display.setCursor(80, 55);
+      if (maraData[0].substring(0, 1) == "C")
+      {
+        display.print("Coffee");
+      }
+      else
+      {
+        display.print("Steam");
+      }   
+    }
+  } 
   else
   {
-    display.print("00");
+    display.fillRect(0, 0, 124, 16, WHITE);
+    display.setCursor(35, 25);
+    display.setTextSize(5);
+    if (seconds < 10)
+    {
+      display.print("0");
+    }
+    display.print(seconds);
   }
-  //Mode
-  display.setTextSize(1);
-  display.setCursor(120, 2);
-  display.print(lastMaraData[0].substring(0, 1));
+
   display.display();
 }
 
 void loop()
 {
   getMaraData();
-  int pumpState = lastMaraData[6].toInt();
+
+  int pumpState = maraData[6].toInt();
   if (pumpState == 1)
   {
-    if ( millis() - lastMillis >= 1000)
+    if (millis() - lastPumpOnMillis >= 1000)
     {
-      lastMillis = millis();
+      lastPumpOnMillis = millis();
       ++seconds;
-      if (seconds > 99)
-        seconds = 0;
     }
   }
   else
   {
-    if (seconds != 0)
-      lastTimer = seconds;
     seconds = 0;
   }
+
   updateView();
 }
